2cc331b2a19999bb4f993aecc40171f7cf5dd20a
aridsisd <aarav4417@hotmail.com>
2025-10-31 16:20:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Point this to your deployed backend (Render or other). Example:
 // window.SEARCH_BACKEND = "https://search-proxy.onrender.com";
-window.SEARCH_BACKEND = "";
+window.SEARCH_BACKEND = "https://backend-mhko.onrender.com";
